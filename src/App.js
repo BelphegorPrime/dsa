95cd13179cea0224bddf45d69ca75f5b2a9ec94a
marcel <roesler1@gmx.de>
2018-06-01 10:08:58 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,181 @@
+import React, { Component, Fragment } from 'react';
+import XmlReader from 'xml-reader';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import Hero from './hero';
+
+const xmlReader = XmlReader.create({
+  stream: false,
+  parentNodes: false,
+  tagPrefix: 'tag:',
+  doneEvent: 'done',
+  emitTopLevelOnly: false
+});
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      heros: [],
+      chosenHero: null,
+      masterMode: false
+    };
+  }
+
+  componentDidMount() {
+    // eslint-disable-next-line no-undef
+    const chosenHero = JSON.parse(window.localStorage.getItem('hero'));
+    this.setState(nextState => ({
+      chosenHero: chosenHero || null,
+      heros: [].concat(...nextState.heros, chosenHero)
+    }));
+  }
+
+  static getDerivedStateFromProps(nextProps, nextState) {
+    if (nextState.chosenHero) {
+      // eslint-disable-next-line no-undef
+      window.localStorage.setItem('hero', JSON.stringify(nextState.chosenHero));
+    }
+    return nextState;
+  }
+
+  appendToState(xml) {
+    this.setState(currentState => {
+      const heros = [].concat(...currentState.heros, xml);
+      const chosenHero = heros.length > 0 ? heros[0] : null;
+      if (chosenHero) {
+        // eslint-disable-next-line no-undef
+        window.localStorage.setItem('hero', JSON.stringify(chosenHero));
+      }
+      return {
+        heros,
+        chosenHero
+      };
+    });
+  }
+
+  fileUploaded(files) {
+    Object.values(files).forEach(file => {
+      // eslint-disable-next-line no-undef
+      const fileReader = new FileReader();
+      fileReader.onload = e => {
+        xmlReader.on('done', data => this.appendToState(data));
+        xmlReader.parse(e.target.result);
+      };
+      fileReader.readAsText(file);
+    });
+  }
+
+  handleChange(val) {
+    this.setState({
+      masterMode: val
+    });
+  }
+
+  chooseHero(name) {
+    this.setState(nextState => ({
+      chosenHero: nextState.heros.filter(
+        h => h.children[0].attributes.name === name
+      )[0]
+    }));
+  }
+
+  render() {
+    const { heros, masterMode, chosenHero } = this.state;
+    const heroOptions = heros.map(hero => ({
+      name: hero.children[0].attributes.name
+    }));
+
+    return (
+      <div className="App">
+        <nav className="navbar navbar-default">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <div className="float-left">
+                <div className="custom-file">
+                  <input
+                    id="validatedCustomFile"
+                    className="custom-file-input"
+                    type="file"
+                    accept="text/xml"
+                    multiple={masterMode}
+                    onChange={e => this.fileUploaded(e.target.files)}
+                  />
+                  <label
+                    className="custom-file-label"
+                    htmlFor="validatedCustomFile">
+                    Held hochladen...
+                  </label>
+                </div>
+              </div>
+              <div className="float-right">
+                <label
+                  id="master-mode-toggle-label"
+                  className="form-check-label"
+                  htmlFor="master-mode-toggle">
+                  Meistermodus
+                </label>
+                <div
+                  id="master-mode-toggle"
+                  className="btn-group btn-group-toggle"
+                  data-toggle="buttons">
+                  <label
+                    className={
+                      !masterMode
+                        ? 'btn btn-secondary active'
+                        : 'btn btn-secondary'
+                    }>
+                    <input
+                      type="radio"
+                      onChange={this.handleChange.bind(this, false)}
+                      checked={!masterMode}
+                    />{' '}
+                    OFF
+                  </label>
+                  <label
+                    className={
+                      masterMode
+                        ? 'btn btn-secondary active'
+                        : 'btn btn-secondary'
+                    }>
+                    <input
+                      type="radio"
+                      onChange={this.handleChange.bind(this, true)}
+                      checked={masterMode}
+                    />{' '}
+                    ON
+                  </label>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+        <div id="app-body" className="row">
+          {masterMode ? (
+            <Fragment>
+              <div className="left-pane col-md-1">
+                {heroOptions.map(h => (
+                  <div
+                    key={h.name}
+                    className="uploaded-hero row text-align-center"
+                    onClick={this.chooseHero.bind(this, h.name)}>
+                    {h.name}
+                  </div>
+                ))}
+              </div>
+              <div className="col-md-11">
+                {chosenHero ? <Hero hero={chosenHero} /> : null}
+              </div>
+            </Fragment>
+          ) : (
+            <div className="col-md-12">
+              {chosenHero ? <Hero hero={chosenHero} /> : null}
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
