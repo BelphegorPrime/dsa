feb7dacf38f8f2787d910a73f98d5233f048fdc7
marcel <roesler1@gmx.de>
2018-06-19 06:49:12 -0700

    idea for houserules

--- src/Rulebook/index.js
+++ src/Rulebook/index.js
@@ -1,6 +1,15 @@
 import lcd from './libreCantionesDeluxe.json';
 
 export default class Rulebook {
+  /**
+   * TODO: export a downloadable File with a template for houserules
+   */
+  constructor(houseRules) {
+    console.log('Rulebook created');
+    this.houseRules = houseRules;
+    console.log(this.houseRules);
+  }
+
   static getLibreCantionesDeluxe() {
     return lcd;
   }
