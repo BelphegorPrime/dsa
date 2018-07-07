0b8412c7b3e11a40927f872b546d7e9dbe0555e7
marcel <roesler1@gmx.de>
2018-07-07 07:12:40 -0700

    finished todo

--- src/Rulebook/index.js
+++ src/Rulebook/index.js
@@ -1,9 +1,6 @@
 import lcd from './libreCantionesDeluxe.json';
 
 export default class Rulebook {
-  /**
-   * TODO: export a downloadable File with a template for houserules
-   */
   constructor(houseRules) {
     this.houseRules = houseRules;
   }
