f0688dfbb2c2c895e6e2ab73e412a49fcc320556
marcel <roesler1@gmx.de>
2018-07-07 06:58:00 -0700

    added spell houserule template

---
+++ public/templates/spellTemplate.js
@@ -0,0 +1,16 @@
+const houseRule = {
+  type: 'spell',
+  // page within the Spell
+  name: 'Ängste Lindern',
+  // page within the LCD
+  page: 23,
+  additionalModification: [
+    {
+      name: 'Panik beruhigen',
+      mod: '+7',
+      minZfW: 10,
+      effect: 'Panische Person wird ruhiger'
+    }
+  ]
+};
+houseRule;
