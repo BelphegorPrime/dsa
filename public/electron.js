32aeb0e2c59f8d3d4a455b8bdb7162ad1d8e7919
marcel <roesler1@gmx.de>
2018-07-14 07:12:14 -0700

    set port to 3000, better electron screen size

--- public/electron.js
+++ public/electron.js
@@ -1,22 +1,16 @@
 const electron = require('electron');
 const path = require('path');
-const url = require('url');
 const isDev = require('electron-is-dev');
 
 const { app, BrowserWindow } = electron;
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({ fullscreen: true, fullscreenable: true });
-  const startUrl =
-    process.env.ELECTRON_START_URL ||
-    url.format({
-      pathname: path.join(__dirname, '../build/index.html'),
-      protocol: 'file:',
-      slashes: true
-    });
+  mainWindow = new BrowserWindow({});
   mainWindow.loadURL(
-    isDev ? startUrl : `file://${path.join(__dirname, '../build/index.html')}`
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
   );
   if (isDev) {
     mainWindow.webContents.openDevTools();
