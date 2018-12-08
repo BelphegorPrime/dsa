7156618e13f7bbbf33037d3b7492b99bb351a864
marcel <roesler1@gmx.de>
2018-12-08 21:03:59 +0100

    refactor entry, add error boundary

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-import React, { StrictMode, ConcurrentMode, lazy, Suspense } from 'react';
+import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import './styles/index.css';
@@ -10,14 +10,46 @@ import Spinner from "./components/Spinner";
 
 const App = lazy(() => import('./components'));
 
+// const ErrorBoundary = props => {
+//     const [hasError, setHasError] = useState(false)
+//     useEffect(() => {
+//         setHasError(true)
+//     }, [hasError]);
+//
+//     if (hasError) {
+//         // You can render any custom fallback UI
+//         return <h1>Something went wrong.</h1>;
+//     }
+//
+//     return props.children;
+// };
+
+class ErrorBoundary extends React.Component {
+    state = { hasError: false };
+
+    static getDerivedStateFromError(error) {
+        console.error('error: ', error);
+        return { hasError: true };
+    }
+
+    componentDidCatch(error, info) {
+        console.error('error: ', error, 'info: ', info);
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return <h1>Something went wrong.</h1>;
+        }
+        return this.props.children;
+    }
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <StrictMode>
-    <ConcurrentMode>
-        <Suspense fallback={<Spinner />}>
-            <App electron={getElectron()} />
-        </Suspense>
-    </ConcurrentMode>
-  </StrictMode>
+  <ErrorBoundary>
+    <Suspense fallback={<Spinner />} maxDuration={500}>
+      <App electron={getElectron()} />
+    </Suspense>
+  </ErrorBoundary>
 );
 registerServiceWorker();
